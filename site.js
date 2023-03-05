feea29f1c14211f05ab4b26ae6b7cf0279b742c4
atothsmi <atothsmi@ucsc.edu>
2023-03-04 21:03:27 -0800

    Puzzle

--- site.js
+++ site.js
@@ -1,7 +1,23 @@
-function search (form){
-    $('checkInputs').show()
+var names = []
+var firstNames = true;
+
+function look(form){
+    
+    $('#searchAppointment').hide();
+    var names = form.names.value;
+    $('#fakeSearch').show();
+    $('#inputedNames').append(names);
+    $('#checkInputs').show();
 }
 
-function testInputs (form) {
-    alert("did it")
+function testInputs(form) {
+    if (form.answer.value == "answer"){
+        $('#incorrect').hide();
+        $('#checkInputs').hide();
+        $('#doneInputs').show();
+    }
+    else {
+        $('#incorrect').show();
+    }
+    
 }
