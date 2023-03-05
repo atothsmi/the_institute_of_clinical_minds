cc47056a10a2cd177085e8cb260ff5f6794f2307
atothsmi <atothsmi@ucsc.edu>
2023-03-04 21:27:05 -0800

    More set-up

--- site.js
+++ site.js
@@ -2,18 +2,25 @@ var names = []
 var firstNames = true;
 
 function look(form){
+    if (form.names.value == 'done'){
+        $('#searchAppointment').hide();
+        $('#doneGame').show();
+    }
+    else {
+        $('#searchAppointment').hide();
+        var names = form.names.value;
+        $('#fakeSearch').show();
+        $('#inputedNames').append(names);
+        $('#checkInputs').show();
+    }
     
-    $('#searchAppointment').hide();
-    var names = form.names.value;
-    $('#fakeSearch').show();
-    $('#inputedNames').append(names);
-    $('#checkInputs').show();
 }
 
 function testInputs(form) {
     if (form.answer.value == "answer"){
         $('#incorrect').hide();
         $('#checkInputs').hide();
+        $('#fakeSearch').hide();
         $('#doneInputs').show();
     }
     else {
